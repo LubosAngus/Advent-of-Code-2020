022d51b03be5137e08e6542f9e55b6301358af0a
anguslubos <angus.lubos@gmail.com>
2020-12-16 00:21:44 +0100

    09 - init

---
+++ 09/01.js
@@ -0,0 +1,14 @@
+console.time('run')
+
+const fs = require('fs')
+const demo = true
+
+fs.readFile(`${demo ? 'demo' : 'input'}.txt`, 'utf8', (err, data) => {
+    if (err) return console.log(err)
+
+    const input = data.trim().split('\n').filter(value => value)
+
+
+
+    console.timeEnd('run')
+})
