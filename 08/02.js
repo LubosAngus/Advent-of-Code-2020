00b25db94855b818a498e372048107f07d373184
anguslubos <angus.lubos@gmail.com>
2020-12-16 00:02:46 +0100

    08 done

---
+++ 08/02.js
@@ -0,0 +1,60 @@
+console.time('run')
+
+const fs = require('fs')
+const demo = false
+
+fs.readFile(`${demo ? 'demo' : 'input'}.txt`, 'utf8', (err, data) => {
+  if (err) return console.log(err)
+
+  const input = data.trim().split('\n').filter(value => value)
+  let run = 0
+  let accumulator = 0
+
+  for (let fixIndex = 0; fixIndex < input.length; fixIndex++) {
+    const fixInstruction = input[fixIndex]
+    const fixSplitted = fixInstruction.split(' ')
+    let wasBreaked = false
+    let fixedInput = [...input]
+
+    if (fixSplitted[0].includes('nop')) {
+      fixedInput[fixIndex] = fixedInput[fixIndex].replace('nop', 'jmp');
+    }
+    else if (fixSplitted[0].includes('jmp')) {
+      fixedInput[fixIndex] = fixedInput[fixIndex].replace('jmp', 'nop');
+    }
+    else {
+      continue
+    }
+
+    accumulator = 0
+
+    for (let index = 0; index < fixedInput.length; index++) {
+      const instruction = fixedInput[index]
+      const splitted = instruction.split(' ')
+
+      run++
+
+      if (splitted[0] == 'xxx') {
+        wasBreaked = true
+        break
+      }
+
+      fixedInput[index] = `xxx | ${fixedInput[index]}`
+
+      if (splitted[0] == 'acc') {
+        accumulator = eval(`${accumulator}${splitted[1]}`)
+      }
+      else if (splitted[0] == 'jmp') {
+        index = eval(`${index}${splitted[1]}-1`)
+      }
+    }
+
+    if (!wasBreaked) {
+      console.table(fixedInput)
+      break
+    }
+  }
+
+  console.log(accumulator)
+  console.timeEnd('run')
+})
