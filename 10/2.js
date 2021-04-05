463574c1e70d56545f8e99a0d785b8664a8243dc
anguslubos <angus.lubos@gmail.com>
2021-04-05 17:19:28 +0200

    day 10 part 2

--- 10/2.js
+++ 10/2.js
@@ -5,42 +5,41 @@ class AdventOfCode extends BaseAdventOfCode
   constructor (inputFileName) {
     super(inputFileName)
 
-    this.possibleDifferences = [1, 2, 3]
+    this.pathsCache = {}
   }
 
   parseInput(data) {
     this.input = data.trim().split('\n').filter(value => value).map(value => parseInt(value)).sort((a, b) => a - b)
     this.input.unshift(0)
-    this.input.push(this.input[this.input.length - 1] + 3)
 
-    return this.input.sort((a, b) => b - a)
+    return this.input
   }
 
-  getNextJoltages(index) {
-    let nextJoltages = []
+  findPath(index) {
+    if (this.input.length - 1 === index) return 1
 
-    for (let i = 0; i < 3; i++) {
-      if (typeof this.input[index + i] === 'undefined') break
-
-      nextJoltages.push(this.input[index + i])
+    if (this.pathsCache[index]) {
+      return this.pathsCache[index]
     }
 
-    return nextJoltages
+    let possiblePaths = 0
+
+    const current = this.input[index];
+
+    for (let j = 1; j <= 3; j++) {
+      const next = this.input[index + j]
+
+      if (typeof next === 'undefined' || next - current > 3) break
+
+      possiblePaths += this.findPath(index + j)
+    }
+
+    this.pathsCache[index] = possiblePaths
+    return possiblePaths
   }
 
   callback() {
-    for (let index = 0; index < this.input.length; index++) {
-      if (typeof this.input[index + 1] === 'undefined') continue
-
-      const currJoltage = this.input[index];
-      const nextJoltages = this.getNextJoltages(index)
-
-      for (let j = 0; j < this.possibleDifferences.length; j++) {
-        if (nextJoltages) continue
-      }
-    }
-
-    return
+    return this.findPath(0)
   }
 }
 
